d2c3af8a1e53883dc49447181f2b7e831449398b
gsandel <Gregory.Sandell@searshc.com>
2014-01-26 08:13:25 -0600

    Add functionality & data for resume page

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -3,6 +3,26 @@ class MainPagesController < ApplicationController
     end
   
     def resume
+        @resume = Resume.new
+        @resume.name = "Greg Sandell"
+        @resume.address.push("3632 S Prairie Avenue")
+        @resume.address.push("Chicago IL 60653")
+        @resume.address.push("773.548.5547")
+        @resume.address.push("greg.sandell &lt;at&gt; gmail.com")
+        @resume.address.push("More detail: http://www.gregsandell.com")
+        @resume.imageJava = "greg-sandell-resume_files/image002.jpg"
+        @resume.imageSCWCD = "greg-sandell-resume_files/image004.jpg"
+        @resume.skillset = ["Work in a software delivery role to provide robust mobile web applications emphasizing development of SaaS web services and RIA interfaces. Seeking positions in downtown Chicago or nearby with minimal travel."]
+        @resume.qual = ["I am a 14-year senior developer of web applications with particular depth in Mobile Web, Javascript, MVC Frameworks, jQuery, Ajax/JSON, web services, Unix, Agile and Open Source. I have worked for Reuters, Chicago Tribune, UBS, Amex, Lockheed-Martin in vertical markets including media, travel, finance, defense, healthcare, eCommerce and education.  I hold a PhD degree and am highly effective at communications, writing and presentations."]
+        position = Position.new(
+            "Sears Holding Corporation", 
+            "Chicago, IL", "Nov 2013 - present",
+			"Contract through Apex Systems", "Developer"
+        )
+        position.listTextArray.push("Member of Core Mobile team supporting sears.com on handheld devices")
+        position.listTextArray.push("Javascript, AngularJS, Git, Grunt")
+        position.listTextArray.push("Converted site to work when device has 'Private Browsing' enabled")
+        @resume.positions.push(position)
     end
   
     def code
@@ -32,14 +52,41 @@ class MainPagesController < ApplicationController
     end
   
   end
-      class Link
-          attr_accessor :text, :url, :remote
-          def initialize(text, url, remote=false) 
-              @text = text;
-              @url = url;
-              @remote = remote
-          end
-          def make_link
-  		    link_to @text, @url
-          end
-      end
+    class Link
+        attr_accessor :text, :url, :remote
+        def initialize(text, url, remote=false) 
+            @text = text;
+            @url = url;
+            @remote = remote
+        end
+    end
+    class Position
+        attr_accessor :company, :location, :dates, :basis, :title, :listTextArray
+        def initialize(company, location, dates, basis, title)
+            @company = company
+            @location = location
+            @dates = dates
+            @basis = basis
+            @title = title
+            @listTextArray = []
+        end
+    end
+    class Resume
+        attr_accessor :positions, :skillset, :qual, :training, :education, :name, :address, :imageJava, :imageSCWCD
+        def initialize
+            @address = []
+            @positions = []
+            @training = []
+            @education = []
+        end
+    end
+    class Training
+        attr_accessor :course, :dates
+        def initialize(course, dates) 
+            @course = course
+            @dates = dates
+        end
+    end
+        
+              
+
