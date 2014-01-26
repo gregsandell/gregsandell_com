099bd5c4e7ac06240bc74f859de48a9136331c25
gsandel <Gregory.Sandell@searshc.com>
2014-01-26 10:00:44 -0600

    Add more resume data; fix auto height of main display

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -23,6 +23,46 @@ class MainPagesController < ApplicationController
         position.listTextArray.push("Javascript, AngularJS, Git, Grunt")
         position.listTextArray.push("Converted site to work when device has 'Private Browsing' enabled")
         @resume.positions.push(position)
+#
+#       PATHFINDER SOFTWARE
+#
+        position = Position.new(
+            "Pathfinder Software", "Chicago, IL", "Aug 2012 - Aug 2013",
+					"Direct Hire", "Sr. Software Developer"
+        )
+        position.listTextArray.push("30-person consultancy specializing in mobie medical solutions");
+        position.listTextArray.push("Integrate jQuery apps with numerous plugins incuding jsViews, headJs, panzoom, fullproof, wysiwyg, moment, showbizPro, bootstrap, colorbox, cookie, easing, dynatree and transit");
+        position.listTextArray.push("Use cloud-based storage providers Parse and Apigee");
+        position.listTextArray.push("Practice Agile principles including scrum, 1-week iterations, Continuous Integration, using Confluence &amp; Pivotal Tracker Git, Github, Jasmine, and Karma");
+        position.listTextArray.push("Write SPA (Single Page Web Applications) using RESTFUL web services plus Ajax and AngularJS");
+        position.listTextArray.push("Delivered a high quality UI for MetroPulse Chicago for exploring a large repository of Chicago civic data; demo video at http://www.youtube.com/watch?v=FfeH4n7k5pA");
+        position.listTextArray.push("Wrote hybrid iOS/web appstore application for the Amer. Assn. for Critical Care Nurses using jQuery Mobile, JSON, AngularJS, Trigger.io, and TestFlight");
+        @resume.positions.push(position)
+#
+#       TRUVEN
+#
+        position = Position.new(
+            "Truven Health Analytics", "Chicago, IL", "Apr 2010 - Aug 2012",
+					"Converted to FT/Perm after contract", "Sr. Software Developer"
+        )
+        position.listTextArray.push("Formerly Thomson-Reuters Healthcare.  Sold in June 2012 for $1.25 bil.");
+        position.listTextArray.push("Create rich, intuitive, high-touch Ajax user interfaces and reusable widgets for CareDiscovery, a  Business Intelligence tool for the Healthcare Industry.");
+        position.listTextArray.push("Create servlet- and portal-based enterprise web applications for IBM Websphere Portal 5, Tomcat 6 & Glassfish in mixed JEE 6 and J2EE 1.4 environment.");
+        position.listTextArray.push("Practice Agile principles including scrum, 3-week iterations, TDD, Continuous Integration");
+        position.listTextArray.push("Develop SaaS components in Java and Ajax communicating over a JSON-based service bus");
+        position.listTextArray.push("Write User Interfaces and Ajax in Javascript & jQuery, promoting reusable patterns, functional programming, and closures");
+        position.listTextArray.push("Develop jQuery plugins for reusable UI widgets");
+        position.listTextArray.push("Integrate Cognos reports into application using Cognos Javascript API and iFrames");
+        position.listTextArray.push("Author PL/SQL queries in Toad that work against a 4 TB Oracle OLAP (Data Warehouse) database, for deployment in Web Services");
+        position.listTextArray.push("Collaborate with the ETL/ Informatica team to determine the necessary data models and UI designs to support Cognos reports.");
+        position.listTextArray.push("Develop Maven plugins and perl scripts to streamline build process");
+        position.listTextArray.push("Work within HIPAA regulations to secure applications and databases using RSA,  TAM, and Oracle Proxy Authentication.");
+        position.listTextArray.push("Break down business requirements into tightly defined software module specifications/estimates &amp; document them on Wiki.");
+        position.listTextArray.push("Write functional prototypes of web interfaces using JSON to simulate Server layer");
+        position.listTextArray.push("Develop portal applications for IBM Websphere Portal Server and deploy with WPS Admin");
+        position.listTextArray.push("Helped transition team from waterfall to agile approach by introducing Maven2, Artifactory Repository Server, Hudson Continuous Integration, Mercurial source control and TDD practices.");
+        position.listTextArray.push("Other technologies not mentioned above: Eclipse, CVS, Rally, JSP, JSTL, Ant, Firebug, Spring MVC/IOC/JDBC, GWT, Struts 1.2, Tiles");
+        @resume.positions.push(position)
     end
   
     def code
